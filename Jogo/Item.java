3a1b0fc09434b96160ac5baab2d4aa2596f7b8e3
Maria Gabriella Xavier Puccinelli <141333033+MariaGabiXavier@users.noreply.github.com>
2025-10-25 22:55:37 -0300

    arrumando erro
    Refactor equals, hashCode, compareTo, and clone methods for better comparison and cloning logic.

--- Jogo/Item.java
+++ Jogo/Item.java
@@ -32,27 +32,59 @@ public class Item implements Comparable<Item>, Cloneable {
     public void decrementar(int q) { setQuantidade(this.quantidade - q); }
 
     @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Item)) return false;
-        Item item = (Item) o;
-        return Objects.equals(nome.toLowerCase(), item.nome.toLowerCase());
+    public boolean equals(Object obj) {
+        if (obj == this) return true;
+        if (obj == null) return false;
+        if (obj.getClass() != this.getClass()) return false;
+
+        Item i = (Item) obj;
+
+        if (!this.nome.equalsIgnoreCase(i.nome)) return false;
+        if (!this.efeito.equalsIgnoreCase(i.efeito)) return false;
+        if (this.quantidade != i.quantidade) return false;
+        if (!this.descricao.equalsIgnoreCase(i.descricao)) return false;
+
+        return true;
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(nome.toLowerCase());
+        int ret = 1;
+
+        ret = ret * 2 + this.nome.toLowerCase().hashCode();
+        ret = ret * 3 + this.descricao.toLowerCase().hashCode();
+        ret = ret * 5 + this.efeito.toLowerCase().hashCode();
+        ret = ret * 7 + ((Integer)this.quantidade).hashCode();
+
+        if (ret < 0) ret = -ret;
+        return ret;
     }
 
     @Override
-    public int compareTo(Item o) {
-        return this.nome.compareToIgnoreCase(o.nome);
+    public int compareTo(Item i) {
+        if (this == i) return 0;
+
+        int cmpNome = this.nome.compareToIgnoreCase(i.nome);
+        if (cmpNome != 0) return cmpNome;
+
+        int cmpEfeito = this.efeito.compareToIgnoreCase(i.efeito);
+        if (cmpEfeito != 0) return cmpEfeito;
+
+        if (this.quantidade < i.quantidade) return -666;
+        if (this.quantidade > i.quantidade) return 666;
+
+        return this.descricao.compareToIgnoreCase(i.descricao);
     }
 
     @Override
     public Item clone() {
-        return new Item(this);
+        try {
+            return (Item) super.clone(); // faz o cast aqui
+        } catch (CloneNotSupportedException e) {
+            throw new AssertionError(); // nunca deve acontecer
+        }
     }
+    
 
     @Override
     public String toString() {
